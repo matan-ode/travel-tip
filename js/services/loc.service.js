d9ef231d8c443be3505f3db531e44d6fbd689283
matan-ode <matanodentz@gmail.com>
2024-10-07 20:21:44 +0300

    Update filter by text and address

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -40,7 +40,7 @@ function query() {
         .then(locs => {
             if (gFilterBy.txt) {
                 const regex = new RegExp(gFilterBy.txt, 'i')
-                locs = locs.filter(loc => regex.test(loc.name))
+                locs = locs.filter(loc => regex.test(loc.geo.address) || regex.test(loc.name))
             }
             if (gFilterBy.minRate) {
                 locs = locs.filter(loc => loc.rate >= gFilterBy.minRate)
